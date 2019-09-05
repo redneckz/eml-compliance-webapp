aab24debe4ca0c403c905f778d2f3a0e296420bc
Alexander Alexandrov <alexander_alexandrov@epam.com>
2019-09-05 15:54:01 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+
+export function App() {
+  return (
+    <div className="app">
+      <div className="flex mb-4">
+        <div className="w-full bg-gray-500 h-12"></div>
+      </div>
+      <div className="flex mb-4">
+        <div className="w-1/2 bg-gray-400 h-12"></div>
+        <div className="w-1/2 bg-gray-500 h-12"></div>
+      </div>
+      <div className="flex mb-4">
+        <div className="w-1/3 bg-gray-400 h-12"></div>
+        <div className="w-1/3 bg-gray-500 h-12"></div>
+        <div className="w-1/3 bg-gray-400 h-12"></div>
+      </div>
+      <div className="flex mb-4">
+        <div className="w-1/4 bg-gray-500 h-12"></div>
+        <div className="w-1/4 bg-gray-400 h-12"></div>
+        <div className="w-1/4 bg-gray-500 h-12"></div>
+        <div className="w-1/4 bg-gray-400 h-12"></div>
+      </div>
+      <div className="flex mb-4">
+        <div className="w-1/5 bg-gray-500 h-12"></div>
+        <div className="w-1/5 bg-gray-400 h-12"></div>
+        <div className="w-1/5 bg-gray-500 h-12"></div>
+        <div className="w-1/5 bg-gray-400 h-12"></div>
+        <div className="w-1/5 bg-gray-500 h-12"></div>
+      </div>
+      <div className="flex">
+        <div className="w-1/6 bg-gray-400 h-12"></div>
+        <div className="w-1/6 bg-gray-500 h-12"></div>
+        <div className="w-1/6 bg-gray-400 h-12"></div>
+        <div className="w-1/6 bg-gray-500 h-12"></div>
+        <div className="w-1/6 bg-gray-400 h-12"></div>
+        <div className="w-1/6 bg-gray-500 h-12"></div>
+      </div>
+    </div>
+  );
+}
