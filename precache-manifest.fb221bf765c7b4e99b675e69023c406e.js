556004ddc2cb63f1fcaed2373dc7d3b3e1e97bfe
Alexander Alexandrov <aalexandrovv@gmail.com>
2019-09-05 14:02:17 +0000

    commit 841480c0baf6b3ce7e3f90dab9a606c6e8dc153c Author: Alexander Alexandrov <alexander_alexandrov@epam.com> Date:   Thu Sep 5 15:54:01 2019 +0300
        Initial commit

---
+++ precache-manifest.fb221bf765c7b4e99b675e69023c406e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e4a8a3da42ac5679a71c995d2a6e3c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0e7640bb11b4fa3ce6b",
+    "url": "/static/css/main.1fabec82.chunk.css"
+  },
+  {
+    "revision": "261973b7788ecd4d335d",
+    "url": "/static/js/2.b5589a9b.chunk.js"
+  },
+  {
+    "revision": "b0e7640bb11b4fa3ce6b",
+    "url": "/static/js/main.eba461c3.chunk.js"
+  },
+  {
+    "revision": "a687e1dea442d3738aaa",
+    "url": "/static/js/runtime~main.f66eb650.js"
+  }
+]);
