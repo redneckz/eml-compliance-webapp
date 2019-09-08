180709ddeaef1c8123c1bd5c4c54637d673334f3
Alexander Alexandrov <aalexandrovv@gmail.com>
2019-09-08 12:42:15 +0300

    Fix useDataPolling inconsistency

--- src/modules/useDataPolling.ts
+++ src/modules/useDataPolling.ts
@@ -3,13 +3,14 @@ import * as React from 'react';
 export function useDataPolling<T>(dataProvider: () => Promise<T>, timeout: number): T | undefined {
   const [data, setData] = React.useState<T>();
   React.useEffect(() => {
+    let timerId: NodeJS.Timeout;
     const updateData = async () => {
       const data = await dataProvider();
       setData(data);
+      timerId = setTimeout(updateData, timeout);
     };
     updateData();
-    const timerId = setInterval(updateData, timeout);
-    return () => clearInterval(timerId);
+    return () => clearTimeout(timerId);
   }, [dataProvider, timeout]);
   return data;
 }
