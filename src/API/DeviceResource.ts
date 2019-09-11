65751a3e135f10c9a8511d344a116a5cac3386ed
Alexander Alexandrov <aalexandrovv@gmail.com>
2019-09-11 19:41:12 +0300

    Fix run test API schema

--- src/API/DeviceResource.ts
+++ src/API/DeviceResource.ts
@@ -4,9 +4,9 @@ import * as Domain from '../Domain';
 
 export const DeviceResource = new Resource<Domain.DeviceStatus>('/list_devices', 'Devices');
 
-const TestService = new Resource<{ 'Wait Time': number }>('/run_test');
+const TestService = new Resource<{ WaitTime: number }>('/run_test');
 
 export async function runTest(): Promise<void> {
-  const { 'Wait Time': timeout } = await TestService.post();
-  await delay((timeout || 5) * 1000);
+  const { WaitTime } = await TestService.post();
+  await delay((WaitTime || 5) * 1000);
 }
